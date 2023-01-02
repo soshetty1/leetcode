4792be2ec0e372ad589b1f8679ce6c3cdf51cbd9
Soumya Shetty <93557088+soshetty1@users.noreply.github.com>
2023-01-02 14:17:57 -0600

    Time: 669 ms (20.35%), Space: 46.4 MB (90.41%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length; i++){
+            
+            int low=i+1;
+            int high= nums.length-1;
+            while(low<high){
+                int sum = nums[i]+ nums[low]+nums[high];
+                
+                if(sum==0){
+                    set.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                    low++;
+                    high--;
+                }
+                
+                else if(sum > 0)
+                    high--;
+                else 
+                    low++;
+            }
+            
+
+            }
+            return new ArrayList<>(set);
+
+        }
+    
+        
+    }
