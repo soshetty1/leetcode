bea17586cc3ac4d0f800d1f694f2f90e1250ccb8
Soumya Shetty <93557088+soshetty1@users.noreply.github.com>
2023-01-19 20:29:32 -0600

    Time: 1 ms (99.36%), Space: 40.2 MB (78.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() %2 !=0 ){
+            return false;
+        }
+        
+  Stack<Character> stack = new Stack<Character>();
+        for(var ch: s.toCharArray())
+        {
+		  if (ch== '(' || ch== '[' || ch== '{'){
+		stack.push(ch);
+		   }
+		    else if (ch== ')' && !stack.isEmpty() && stack.peek() =='(') {
+		stack.pop();	
+        }
+		else if(ch== ']' && !stack.isEmpty() && stack.peek() =='[') {
+		stack.pop();	
+		}
+		else if(ch== '}' && !stack.isEmpty() && stack.peek() =='{') {
+		stack.pop();	
+		}
+            else{
+                return false;
+            }
+	}
+return stack.isEmpty();
+    }
+}
